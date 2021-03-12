f76cd432e66c6de8fd9c54bd94c68f5c1b23fad8
Jelle <46423958+Jelle-M@users.noreply.github.com>
2021-03-12 03:53:53 +0100

    Update record.bat

--- record.bat
+++ record.bat
@@ -1,13 +1,21 @@
 @echo off
 K: & cd videos/criticalrole
 set WAIT=1
+set DURATION=12600
 set CHANNEL=criticalrole
 set CUR_YYYY=%date:~10,4%
 set CUR_MM=%date:~4,2%
 set CUR_DD=%date:~7,2%
 set CUR_HH=%time:~0,2%
 set FILEDATE=%CUR_YYYY%-%CUR_MM%-%CUR_DD%
-set AUTH_TOKEN=v15mfub562jxtj2hapqu640vaogegt 
+goto endofcomment
+    change parameters above^: 
+	use cd to navigate to directory where to save recording
+	WAIT=amount of seconds until start recording
+	EPISODE=episode counter
+        DURATION=duration of recording in seconds. (6=21600, 5=18000, 4.5=16200)
+	insert line "shutdown -s -f -t %DURATION%" after timeout to limit recording time
+:endofcomment
 title Recording - %CHANNEL% %FILEDATE%
 timeout /T %WAIT% /NOBREAK
 set COUNTER=1
