98c940670189a6efb33d8982126589dc0825dc55
Jelle Meeus <jellemeeus@hotmail.com>
2024-04-07 20:43:51 +0200

    update windows bat

--- record.bat
+++ record.bat
@@ -1,26 +1,40 @@
 @echo off
-K: & cd videos/criticalrole
+
+REM Set working directory
+cd /D K:\videos\criticalrole
+
+REM Set parameters
 set WAIT=1
 set DURATION=12600
 set CHANNEL=criticalrole
+
+REM Get current date
 set CUR_YYYY=%date:~10,4%
-set CUR_MM=%date:~4,2%
+set CUR_MM=%date:~4,2%  
 set CUR_DD=%date:~7,2%
+
+REM Get current time
 set CUR_HH=%time:~0,2%
+
+REM Build filename 
 set FILEDATE=%CUR_YYYY%-%CUR_MM%-%CUR_DD%
-goto endofcomment
-    change parameters above^: 
-	use cd to navigate to directory where to save recording
-	WAIT=amount of seconds until start recording
-	EPISODE=episode counter
-        DURATION=duration of recording in seconds. (6=21600, 5=18000, 4.5=16200)
-	insert line "shutdown -s -f -t %DURATION%" after timeout to limit recording time
-:endofcomment
-title Recording - %CHANNEL% %FILEDATE%
+
+REM Set title
+title Recording - %CHANNEL% %FILEDATE% 
+
+REM Wait before recording
 timeout /T %WAIT% /NOBREAK
+
+REM Start loop to record multiple files
 set COUNTER=1
 :loop
 streamlink twitch.tv/%CHANNEL% best -o "%CHANNEL%_%FILEDATE%_%COUNTER%.flv" --force -O --retry-streams 30 --retry-open 9999 --twitch-disable-hosting
 timeout 30
-set /A COUNTER=COUNTER+1
-goto loop
+set /A COUNTER+=1
+if %COUNTER% LEQ 10 goto loop
+
+REM Add shutdown after recording if needed
+REM shutdown -s -f -t %DURATION%
+
+:endofcomment
+
