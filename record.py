706f9135dc7e97fb128af913a6d9c6a58e770497
Jelle Meeus <jellemeeus@hotmail.com>
2024-04-07 20:44:11 +0200

    update py

--- record.py
+++ record.py
@@ -1,11 +1,14 @@
 """ Record stream """
-# !/usr/bin/env python3
-import argparse
-import datetime as dt
-import os
-import time
 from contextlib import contextmanager
 from pathlib import Path
+import argparse
+import datetime as dt
+import logging
+import os
+import time
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.INFO)
 
 
 def try_except_default(func):
@@ -20,42 +23,57 @@ def try_except_default(func):
 
 
 @try_except_default(0)
-def seconds_until_criticalrole():
+def get_seconds_until_criticalrole() -> int | None:
     """ Calculate seconds until critical role
     if anything goes wrong, default to 0 seconds """
+    logger.info("Using selenium to get seconds until CR airs")
     from selenium import webdriver
+    from selenium.webdriver.common.by import By
     from selenium.webdriver.firefox.options import Options
-    my_url = 'http://wheniscriticalrole.com'
+    my_url = "http://wheniscriticalrole.com"
     options = Options()
     options.headless = True
-    driver = webdriver.Firefox(options=options)
-    driver.get(my_url)
-    days = int(driver.find_element_by_id(id_='days').text)
-    hours = int(driver.find_element_by_id(id_='hours').text)
-    minutes = int(driver.find_element_by_id(id_='minutes').text)
-    seconds = int(driver.find_element_by_id(id_='seconds').text)
-    print(
-        f'CR Airs in: {days} days, {hours} hours,'
-        '{minutes} minutes, {seconds} seconds')
-    return days * 24 * 3600 + hours * 3600 + minutes * 60 + seconds
+    with webdriver.Firefox(options=options) as driver:
+        driver.get(my_url)
+        try:
+            days = int(driver.find_element(By.ID, "days").text)
+            hours = int(driver.find_element(By.ID, "hours").text)
+            minutes = int(driver.find_element(By.ID, "minutes").text)
+            seconds = int(driver.find_element(By.ID, "seconds").text)
+            logger.info(
+                f"CR Airs in: {days} days, {hours} hours, {minutes} minutes, {seconds} seconds")
+            seconds_until_criticalrole = days * 24 * \
+                3600 + hours * 3600 + minutes * 60 + seconds
+            return seconds_until_criticalrole
+        except ValueError:
+            logger.error("Failed to parse website")
+            return None
 
 
 def parse_args():
     """ Parse args """
-    parser = argparse.ArgumentParser(description='Process arguments')
+    parser = argparse.ArgumentParser(description="Process arguments")
     parser.add_argument(
-        '-u', '--url', dest='url', default='twitch.tv/criticalrole',
-        help='url to twitch channel')
+        "-u", "--url", dest="url", default="twitch.tv/criticalrole",
+        help="url to twitch channel"
+    )
     parser.add_argument(
-        '-o', '--out', dest='out_dir',
-        default=f'/home/{os.getlogin()}/Videos/criticalrole/',
-        help='directory where to save video')
+        "-o", "--out", dest="out_dir",
+        default=Path.home(),
+        help="directory where to save video"
+    )
     parser.add_argument(
-        '-w', '--wait', dest='time_wait', type=int,
-        help='wait time (seconds) before start recording')
+        "-s", "--sleep", dest="sleep_time", type=int,
+        help="time to sleep in seconds before recording starts"
+    )
     parser.add_argument(
-        '-n', '--now', dest='now', action="store_true",
-        help='start recording right now')
+        "-n", "--now", dest="now", action="store_true",
+        help="start recording right now"
+    )
+    parser.add_argument(
+        "--offset", type=int, default=5,
+        help="amount of minutes to start recording before stream is online. Default is 5"
+    )
     return parser.parse_args()
 
 
@@ -72,33 +90,46 @@ def working_directory(path):
 
 def record_worker(url):
     """ Call streamlink to record """
+    failure_count = 0
     while True:
         time_now = dt.datetime.now()
         cmd = (
-            f'streamlink {url} best -o '
-            f'"{time_now:%Y-%m-%d_%H-%M-%S}.flv" '
-            '--force -O --retry-streams 30 --retry-open 9999'
+            f"streamlink {url} best -o \"{time_now: %Y-%m-%d_ %H-%M-%S}.flv\" "
+            + "--force -O --retry-streams 30 --retry-open 9999"
         )
         os.system(cmd)
+        failure_count += 1
+        if failure_count >= 10: # Assume stream is offline if it fails 10 times in a row
+            logger.info("Stream offline or failing to record")
+            break
         time.sleep(30)
 
 
 def main(args):
     """ Set up worker to record CR """
-    if not args.now:
-        if args.time_wait is not None:
-            sleep_duration = args.time_wait
-        else:
-            sleep_duration = seconds_until_criticalrole()-10*60
-            if sleep_duration == 0:
-                print('WARNING: Failed to parse website! Input time with -w')
-        print(f'Sleep for {sleep_duration} seconds!')
-        time.sleep(sleep_duration)
+    # Change to the output directory
     with working_directory(Path(args.out_dir)):
-        print('Start recording')
+        # If not starting immediately, calculate the sleep time
+        if not args.now:
+            if args.sleep_time is not None:
+                sleep_duration = args.sleep_time
+            else:
+                # Calculate seconds until CR starts, subtract offset
+                sleep_duration = max(
+                    0, get_seconds_until_criticalrole()-(args.offset*60))
+                if sleep_duration is None:
+                    # Log warning if sleep time can't be calculated
+                    logger.warning(
+                        "Failed to parse website! Input time with -w instead")
+            # Log the sleep time
+            logger.info(f"Sleep for {sleep_duration:,} seconds")
+            time.sleep(sleep_duration)
+        # Log a message to indicate recording is starting
+        logger.info("Start recording")
+        # Call the record worker function
         record_worker(args.url)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     ARGS = parse_args()
     main(ARGS)
